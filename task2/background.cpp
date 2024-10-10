9889d755e467afecbfc62efea3e85f612bb67906
Dezzleshipc <foler126@gmail.com>
2024-10-10 14:24:34 +1000

    task2 unix

--- task2/background.cpp
+++ task2/background.cpp
@@ -1,49 +1,73 @@
 #ifdef _WIN32
-    #include <windows.h>
+
+#include <windows.h>
+
 #else
+
+#include <spawn.h>
+#include <wait.h>
+#include <string.h>
 
 #endif
 
-#include <iostream>
-#include <filesystem>
-
+// #include <iostream>
 #include "background.hpp"
 
-int signed start_background(const char* program_path) {
+int signed start_background(const char *program_path)
+{
 
-    #ifdef _WIN32
-    
-    STARTUPINFO si; 
+#ifdef _WIN32
+
+    STARTUPINFO si;
     PROCESS_INFORMATION pi;
-    CreateProcess( program_path,   // the path
-        NULL,        // Command line
-        NULL,           // Process handle not inheritable
-        NULL,           // Thread handle not inheritable
-        FALSE,          // Set handle inheritance to FALSE
-        0,              // No creation flags
-        NULL,           // Use parent's environment block
-        NULL,           // Use parent's starting directory 
-        &si,            // Pointer to STARTUPINFO structure
-        &pi             // Pointer to PROCESS_INFORMATION structure (removed extra parentheses)
+    CreateProcess(program_path, // the path
+                  NULL,         // Command line
+                  NULL,         // Process handle not inheritable
+                  NULL,         // Thread handle not inheritable
+                  FALSE,        // Set handle inheritance to FALSE
+                  0,            // No creation flags
+                  NULL,         // Use parent's environment block
+                  NULL,         // Use parent's starting directory
+                  &si,          // Pointer to STARTUPINFO structure
+                  &pi           // Pointer to PROCESS_INFORMATION structure (removed extra parentheses)
     );
 
-    std::cout << "Process id: " << pi.dwProcessId << "\n";
+    // std::cout << "Process id: " << pi.dwProcessId << "\n";
     return pi.dwProcessId;
 
-    #else
-    return 0;
+#else
+    int status;
 
-    #endif
+    pid_t pid;
+    char *argv[] = {};
+    status = posix_spawn(
+        &pid,
+        program_path,
+        NULL,
+        NULL,
+        argv,
+        NULL);
+
+    // std::cout << strerror(status) << "\n";
+
+    return pid;
+#endif
 }
 
-int wait_program(const int signed pid) {
-    #ifdef _WIN32
-    
+int wait_program(const int signed pid)
+{
+#ifdef _WIN32
+
     HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
     return WaitForSingleObject(handle, INFINITE);
 
-    #else
-    return 0;
+#else
 
-    #endif
+    int status;
+    waitpid(pid, &status, 0);
+
+    // std::cout << status << " "<< strerror(status) <<"\n";
+    return status;
+
+#endif
 }
--- task2/main.cpp
+++ task2/main.cpp
@@ -1,14 +1,14 @@
 #include "background.hpp"
 #include <iostream>
-#include <filesystem>
 
 int main(int argc, char **argv)
 {
-    #ifdef _WIN32
+#ifdef _WIN32
     const auto path = "C:\\Programs\\Github\\Operating-systems\\task2\\subprogram.exe";
-    #else
-    const auto path = "";
-    #endif
+#else
+    const auto path = "/home/dezz/Github/Operating-systems/task2/subprogram";
+#endif
+
     std::cout << path << "\n";
     auto pid = start_background(path);
     auto exit_code = wait_program(pid);
